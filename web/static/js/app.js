2fd84fdf7869948fea7c7eb9ca7f59af70c4d6b7
Bryan Joseph <bryanjos@gmail.com>
2015-03-22 21:07:42 -0500

    Renamed app.jsx to app.js

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -6,17 +6,20 @@ import Dispatcher from "./dispatcher";
 import Router from "./components/router";
 import Channels from "./channels";
 
-window.App = {};
-window.App.Dispatcher = Dispatcher;
-window.App.Channels = Channels;
-
-window.App.getToken = function(){
-  return App.Dispatcher.getStore("auth").data.token;
+let App = {
+  Dispatcher: Dispatcher,
+  Channels: Channels,
+  getToken: function(){
+    return this.Dispatcher.getStore("auth").data.token;
+  },
+  Socket: null
 }
+
+window.App = App;
 
 if(App.getToken()){
   App.Socket = App.Channels.createSocket();
-  App.Channels.init(window.App.Socket);
+  App.Channels.init(App.Socket);
 }
 
 Router.start();
